a584ca5cd675dc91f348fcbc2a1467bf91d8b0c7
brett-anderson <brettanderson16@gmail.com>
2013-12-13 17:06:36 -0600

    Install blank rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Classified::Application.config.secret_token = '17a9dc3e7e2d44d32c97b1caf9cd358d1da2ada14c27bde89b3f3cac828b2c7c1024eafc4f2544a832309f00eab8b8042223c2d560295a527db4f8f3fe406202'
